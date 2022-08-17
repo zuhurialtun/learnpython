7b09590d6181cb67685c5837b6c708cf5bb394ef
zuhurialtun <zuhuri312@gmail.com>
2022-08-17 11:42:28 +0300

    Find big word and word count with text file.

---
+++ home.py
@@ -0,0 +1,21 @@
+from distutils import dist
+from itertools import count
+
+
+name = input('Enter File:')
+handle  = open(name)
+
+counts = dict()
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word,0) + 1
+
+bigcount = None 
+bigword = None 
+for word,count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigword = word
+        bigcount = count
+
+print(bigword,bigcount)
