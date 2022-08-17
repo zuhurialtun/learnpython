2b4f2df5d821aa883e7ba532f88566ae127e045e
zuhurialtun <zuhuri312@gmail.com>
2022-08-17 17:05:08 +0300

    Maybe finish.

--- rockpaperscissors/rps.py
+++ rockpaperscissors/rps.py
@@ -1,4 +1,6 @@
 import random
+from unittest import result
+
 
 def get_choices():
     options = ['rock', 'paper', 'scissors']
@@ -7,5 +9,27 @@ def get_choices():
     choices = {'player': player_choice, 'computer': computer_choice}
     return choices
 
+
+def check_win(player, computer):
+    print(f'You chose {player}, computer chose {computer}')
+    if player == computer:
+        return "It's a tie!"
+    elif player == 'rock':
+        if computer == 'paper':
+            return "You lose."
+        else:
+            return "You win!"
+    elif player == 'paper':
+        if computer == 'rock':
+            return "You win!"
+        else:
+            return "You lose."
+    elif player == 'scissors':
+        if computer == 'paper':
+            return "You win"
+        else:
+            return "You lose."
+
 choices = get_choices()
-print(choices)
+winner = check_win(choices['player'],choices['computer'])
+print(winner)
